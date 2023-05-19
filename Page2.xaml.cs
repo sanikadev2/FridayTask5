42f874b58dde2ba06ca0847dd88fd0539c053ba5
sanikadev2 <117445346+sanikadev2@users.noreply.github.com>
2023-05-19 17:41:04 +0530

    Add files via upload

---
+++ Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public ObservableCollection<CarouselItem> CarouselItems { get; set; }
+
+        public Page2()
+        {
+            InitializeComponent();
+            CarouselItems = new ObservableCollection<CarouselItem>()
+        {
+            new CarouselItem() {ImageSource = "Frame" },
+            new CarouselItem() { ImageSource = "Frame" },
+            new CarouselItem() { ImageSource = "Frame" }
+        };
+
+            BindingContext = this;
+        }
+    }
+    public class CarouselItem
+    {
+        public string Title { get; set; }
+        public string ImageSource { get; set; }
+    }
+}
+        
+        
